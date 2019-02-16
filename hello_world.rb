d4f9f0351e741d67349c038ce6850777b6cf66e3
astonishing-panel-1637 <dr.erbil@protonmail.ch>
2019-02-16 17:24:05 +0000

    Automatically backed up by Learn

--- hello_world.rb
+++ hello_world.rb
@@ -1,4 +1,10 @@
-message = "Hello world " + " great to meet you!"
+class_name = "Febuary"
 
 def say_message(name)
+  message = "Hello " + name + " Great to meet you!"
+end
+
+say_message(class_name)
+
+
   
