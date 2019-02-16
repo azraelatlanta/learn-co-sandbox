e42b39a991bbb205e6a01e590ad6a795ca5e7d2a
astonishing-panel-1637 <dr.erbil@protonmail.ch>
2019-02-16 17:21:52 +0000

    Automatically backed up by Learn

--- hello_world.rb
+++ hello_world.rb
@@ -1,16 +1,4 @@
-#puts "Hello world"
-#puts "Hello world".upcase
-#puts "Hello world".downcase
+message = "Hello world " + " great to meet you!"
 
-greeting = "Hello Atlanta Fasttrack"
-puts greeting.upcase
-puts greeting.downcase
-puts greeting * 1000
-
-def say_message
-  message = "Hello world" + "Great to meet you!"
-  puts message
-end
-
-say_message
-say_message
+def say_message(name)
+  
